69edd7c6ec73f59a9b143e101b0add9381bd038a
Ale Puppi <guillenn2002@gmail.com>
2025-05-03 17:08:22 -0500

    Corrige import en ClienteModal.jsx con nombre correcto de CSS

--- warrior-frontend/src/components/ClienteModal.jsx
+++ warrior-frontend/src/components/ClienteModal.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-import "./ClienteModal.css";
+import "./clientemodal.css";
 
 const ClienteModal = ({ setMostrarModal }) => {
   const [formulario, setFormulario] = useState({
