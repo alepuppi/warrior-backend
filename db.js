4a354e2c1a40157646796563aa3404c05bdedf8e
Ale Puppi <guillenn2002@gmail.com>
2025-06-20 16:39:19 -0500

    fix: ensure DB_PORT is parsed as number

--- db.js
+++ db.js
@@ -6,7 +6,7 @@ console.log("🔍 DB_USER desde db.js:", process.env.DB_USER);
 
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    port: Number(process.env.DB_PORT),
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
