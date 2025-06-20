ec9b27992ccec8acd51998a042f4e48cfab896af
Ale Puppi <guillenn2002@gmail.com>
2025-06-20 18:00:10 -0500

    Fix DB_PORT parseo correcto y debug

--- db.js
+++ db.js
@@ -8,10 +8,10 @@ console.log("🔍 DB_USER desde db.js:", process.env.DB_USER);
 console.log("🌐 DB_HOST:", process.env.DB_HOST);
 console.log("🔌 DB_PORT:", process.env.DB_PORT);
 console.log("🗄️ DB_NAME:", process.env.DB_NAME);
-
+console.log('✅ Pool de conexión creado con:');
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
+    port: parseInt(process.env.DB_PORT || "3306", 10),
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
